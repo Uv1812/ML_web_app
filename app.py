db50cc5ea9bfeb08e287c6598c9c2c99a4ad5c3e
Urvisha Gothadiya <148183929+Uv1812@users.noreply.github.com>
2025-08-05 08:11:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ import numpy as np
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-dia_mod=pickle.load(open("D:/ML/web file/model/trained_model.sav",'rb'))
-ht_mod=pickle.load(open("D:/ML/web file/model/heart_model.sav",'rb'))
-par_mod=pickle.load(open("D:/ML/web file/model/parkinson_pre.sav",'rb'))
+dia_mod=pickle.load(open("D:/ML/web file/model/trained_model.sav"))
+ht_mod=pickle.load(open("D:/ML/web file/model/heart_model.sav"))
+par_mod=pickle.load(open("D:/ML/web file/model/parkinson_pre.sav"))
 
 with st.sidebar:
     selected=option_menu('Multiplr Disease Prediction System',
@@ -185,3 +185,4 @@ if(selected=='Parkinsons Prediction'):
             diagnosis="The person has no Parkinson's  disease"
 
     st.success(diagnosis)
+
